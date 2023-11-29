5e4effdb8b2553928595623e782e8943f6d0e615
David <davidbujosa@gmail.com>
2023-11-29 16:29:37 -0400

    feat: Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 
+import requests
+
+base_url = "https://graph.instagram.com"
+access_token = "YOUR TOKEN"
+
+# Endpoint to get the feed
+endpoint = "/me/media"
+url = f"{base_url}{endpoint}"
+
+# Request parameters
+params = {
+    "fields": "id,caption,media_type,media_url,thumbnail_url,permalink,timestamp",
+    "access_token": access_token
+}
+
+# Make the GET request
+response = requests.get(url, params=params)
+
+# Check the status of the response
+if response.status_code == 200:
+    # The request was successful, process the data
+    data = response.json()
+    print(data)
+else:
+    # The request failed, display the status code and error message
+    print(f"Error: {response.status_code} - {response.text}")
